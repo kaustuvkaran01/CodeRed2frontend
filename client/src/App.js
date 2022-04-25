940e67e49c404ffce0cc5207099f5751a279cd3c
kkaran-be18 <kkaran_be18@thapar.edu>
2022-04-25 15:34:02 +0530

    init: Initialized the client

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import './App.css';
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
